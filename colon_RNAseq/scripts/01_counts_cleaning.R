57724e0447549c475de7a6b3ef5b260c2b7d2ef9
jakesboles <jake.boles@ufl.edu>
2023-08-07 20:54:04 -0400

    colon 01

--- colon_RNAseq/scripts/01_counts_cleaning.R
+++ colon_RNAseq/scripts/01_counts_cleaning.R
@@ -6,3 +6,8 @@ library(tidyverse)
 con <- gzcon(url("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE239820&format=file&file=GSE239820%5Fraw%5Fcounts%2Ecsv%2Egz"))
 csv <- readLines(con)
 counts <- read.csv(textConnection(csv))
+
+counts <- counts[, mixedsort(colnames(counts))] %>%
+  column_to_rownames(var = "X") %>%
+  mutate_all(function(x) as.numeric(x)) %>%
+  dplyr::select(JB266:JB395) #these are the brain samples 
