f98e7274e24533681918fd4be3f702473fbe1ded
jakesboles <jake.boles@ufl.edu>
2023-08-07 20:16:49 -0400

    Corrected metadata

--- IGNORE/00_metadata_creation.R
+++ IGNORE/00_metadata_creation.R
@@ -3,12 +3,11 @@ library(tidyverse)
 
 attr <- read.csv("traits.csv")
 attr <- attr[,c(1:3)]
-attr$group <- factor(attr$group, 
-                     levels = c("Naïve", "1", "2", "3", "4", "5", "6"),
-                     labels = c("Untreated", "5d_DSS", "7d_DSS", "7d_DSS_2d_H2O",
+attr$group <- factor(attr$group,
+                     levels = c("Untreated", "5d_DSS", "7d_DSS", "7d_DSS_2d_H2O",
                                 "7d_DSS_5d_H2O", "7d_DSS_7d_H2O", "7d_DSS_14d_H2O"))
 
-key <- read_xlsx("9441.xlsx")
+key <- read_xlsx("IGNORE/9441.xlsx")
 key <- key %>%
   separate(col = "Sample ID",
            into = c("id", "tissue"), sep = "_") %>%
@@ -20,12 +19,13 @@ key <- key %>%
     tissue == "H" ~ "hippocampus",
     tissue == "D" ~ "distal_colon",
     tissue == "P" ~ "proximal_colon"
-  ))
+  )) %>%
+  distinct(FileID, .keep_all = T)
 
 gut <- key[c(266:nrow(key)), ]
 brain <- key[c(1:265), ]
 
-write.csv(brain, file = "RNAseq_brain_metadata.csv",
+write.csv(brain, file = "brain_RNAseq/RNAseq_brain_metadata.csv",
           row.names = F)
-write.csv(gut, file = "RNAseq_colon_metadata.csv",
+write.csv(gut, file = "colon_RNAseq/RNAseq_colon_metadata.csv",
           row.names = F)
