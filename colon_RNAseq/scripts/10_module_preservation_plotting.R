45281154f4ac44f603619e2c2a0465a9b2a550a6
jakesboles <jake.boles@ufl.edu>
2023-08-09 16:48:14 -0400

    update 11 decided to do preservation in separate script after all

--- colon_RNAseq/scripts/10_module_preservation_plotting.R
+++
@@ -1,2 +0,0 @@
-#This script visualizes the module preservation results in one heatmap, focusing only on modules of interest
-#Interesting modules were identified in 07
---
+++ colon_RNAseq/scripts/11_module_preservation_plotting.R
@@ -0,0 +1,14 @@
+#This script visualizes the module preservation results in one heatmap, focusing only on modules of interest
+#Interesting modules were identified in 07
+
+library(WGCNA)
+library(tidyverse)
+library(ggplot2)
+library(paletteer)
+library(ggpp)
+library(forcats)
+library(pheatmap)
+library(ggh4x)
+library(cowplot)
+
+load("colon_RNAseq/data_objects/module_preservation.RData")
