445419351d34f3547226dcda6d67616f9ab2b308
jakesboles <jake.boles@ufl.edu>
2023-08-16 10:40:58 -0400

    added code to build the needed gene set collections

--- brain_RNAseq/scripts/14_anRichment.R
+++ brain_RNAseq/scripts/14_anRichment.R
@@ -1,2 +1,122 @@
 #This script uses the anRichment package to infer biological function for each module
 #This script will also generate the horizontal bar charts displaying these results
+
+library(WGCNA)
+library(anRichment)
+
+options(stringsAsFactors = F)
+
+load("brain_RNAseq/consensus_WGCNA_input.RData")
+load("brain_RNAseq/data_objects/consensus_network.RData")
+
+symbol <- colnames(multiExpr[[1]]$data)
+
+entrez <- convert2entrez(organism = "Mus musculus", symbol = symbol)
+
+GOcollection <- buildGOcollection(organism = "mouse")
+knownGroups(GOcollection)
+GO.BPcollection <- subsetCollection(GOcollection, tags = "GO.BP")
+
+#subsetting GO.BP collection to remove huge gene lists----
+idx <- vector(mode = "logical", length = length(GO.BPcollection$dataSets))
+
+max_genes <- 500
+
+for (i in seq_along(GO.BPcollection$dataSets)){
+  idx[i] <- nrow(GO.BPcollection$dataSets[[i]]$data) < max_genes
+}
+
+small_go <- GO.BPcollection$dataSets[idx]
+{
+  dataSets <- list()
+  
+  for (i in seq_along(small_go)){
+    go_entrez <- small_go[[i]]$data$Entrez
+    go_evidence <- small_go[[i]]$data$evidence
+    go_source <- small_go[[i]]$data$source
+    
+    dataSets[i] <- newGeneSet(
+      geneEntrez = go_entrez,
+      geneEvidence = go_evidence,
+      geneSource = go_source,
+      ID = small_go[[i]]$ID,
+      name = small_go[[i]]$name,
+      description = small_go[[i]]$description,
+      source = small_go[[i]]$source,
+      organism = "mouse",
+      internalClassification = small_go[[i]]$internalClassification,
+      groups = small_go[[i]]$groups,
+      lastModified = small_go[[i]]$lastModified
+    )
+  }
+  
+  names(dataSets) <- names(small_go)
+}
+
+small_go_group <- newGroup(name = "GO.BP",
+                           description = paste0("GO-BP knowledgebase gene sets with fewer than ", max_genes, " genes"),
+                           source = "GO")
+{
+  smallGOcollection <- newCollection(groups = list(small_go_group))
+  smallGOcollection$dataSets <- vector(mode = "list", length = length(dataSets))
+  
+  for (i in seq_along(dataSets)){
+    smallGOcollection$dataSets[[i]]$data <- data.frame(Entrez = dataSets[[i]]$Entrez,
+                                                       evidence = dataSets[[i]]$evidence,
+                                                       source = dataSets[[i]]$source)
+    
+    names(smallGOcollection$dataSets)[[i]] <- names(dataSets)[[i]]
+    
+    smallGOcollection$dataSets[[i]]$ID <- small_go[[i]]$ID
+    
+    smallGOcollection$dataSets[[i]]$name <- small_go[[i]]$name
+    
+    smallGOcollection$dataSets[[i]]$shortName <- small_go[[i]]$shortName
+    
+    smallGOcollection$dataSets[[i]]$description <- small_go[[i]]$description
+    
+    smallGOcollection$dataSets[[i]]$source <- small_go[[i]]$source
+    
+    smallGOcollection$dataSets[[i]]$organism <- small_go[[i]]$organism
+    
+    smallGOcollection$dataSets[[i]]$internalClassification <- small_go[[i]]$internalClassification
+    
+    smallGOcollection$dataSets[[i]]$groups <- small_go[[i]]$groups
+    
+    smallGOcollection$dataSets[[i]]$lastModified <- small_go[[i]]$lastModified
+    
+    smallGOcollection$dataSets[[i]]$alternateNames <- small_go[[i]]$alternateNames
+    
+    smallGOcollection$dataSets[[i]]$externalDB <- small_go[[i]]$externalDB
+    
+    smallGOcollection$dataSets[[i]]$externalAccession <- small_go[[i]]$externalAccession
+    
+    smallGOcollection$dataSets[[i]]$webLink <- small_go[[i]]$webLink
+    
+    smallGOcollection$dataSets[[i]]$weightIndex <- small_go[[i]]$weightIndex
+    
+    smallGOcollection$dataSets[[i]]$type <- small_go[[i]]$type
+  }
+}
+
+#Build other collections
+biosysCollection <- BioSystemsCollection("mouse")
+knownGroups(biosysCollection)
+keggCollection <- subsetCollection(biosysCollection, tags = "KEGG",
+                                   matchComponents = "groups")
+biocycCollection <- subsetCollection(biosysCollection, tags = "BIOCYC",
+                                     matchComponents = "groups")
+reactomeCollection <- subsetCollection(biosysCollection, tags = "REACTOME",
+                                       matchComponents = "groups")
+
+msdbCollection <- MSigDBCollection(file = "msigdb_v2023.1.Mm.xml/msigdb_v2023.1.Mm.xml", organism = "mouse")
+mhCollectrion <- subsetCollection(msdbCollection,
+                                  tags = "MSigDB MH: NA")
+wikiCollection <- subsetCollection(msdbCollection,
+                                   tags = "MSigDB M2: NA - CP:WIKIPATHWAYS")
+biocartaCollection <- subsetCollection(msdbCollection,
+                                       tags = "MSigDB M2: NA - CP:BIOCARTA")
+
+combinedCollection <- mergeCollections(smallGOcollection, biocartaCollection,
+                                       keggCollection, mhCollectrion,
+                                       reactomeCollection, wikiCollection)
