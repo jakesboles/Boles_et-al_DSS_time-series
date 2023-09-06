db92cc61ede66b9c0cf867f7506e806297e5cb5e
jakesboles <jake.boles@ufl.edu>
2023-09-06 13:57:39 -0400

    brain 10 done

--- brain_RNAseq/scripts/10_consensus_preservation.R
+++ brain_RNAseq/scripts/10_consensus_preservation.R
@@ -1,3 +1,23 @@
 #This script calculates module preservation statistics for the consensus modules found in the WGCNA
 #Effectively, we're evaluating how similar the modules are between one network (i.e., tissue) and another
 #This analysis is wrapped neatly in the WGCNA package
+library(WGCNA)
+library(rstudioapi)
+
+load(file = "brain_RNAseq/data_objects/consensus_WGCNA_input.RData")
+load(file = "brain_RNAseq/data_objects/consensus_network.RData")
+
+exprSize <- checkSets(multiExpr)
+nSets <- exprSize$nSets
+
+consMEsC <- multiSetMEs(multiExpr, universalColors = moduleColors)
+MET <- consensusOrderMEs(consMEsC)
+
+sizeGrWindow(8, 10)
+par(cex = 0.9)
+plotEigengeneNetworks(MET, setLabels, marDendro = c(0, 2, 2, 1),
+                      marHeatmap = c(3, 3, 2, 1),
+                      zlimPreservation = c(0.5, 1), xLabelsAngle = 90)
+savePlotAsImage(file = "brain_RNAseq/plots/brain_consensus_networks_preservation.png", 
+                format = "png",
+                height = 1000, width = 1200)
